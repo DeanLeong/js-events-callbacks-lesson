a1ef75e1806076f5485bb77ec97e5b7d580ee83e
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-09-16 09:12:35 -0600

    solution

--- color-scheme-switcher/js/main.js
+++ color-scheme-switcher/js/main.js
@@ -1,14 +1,42 @@
 // First, select each button.
 
+// const redButton = document.querySelector('.red')
 
+// const whiteButton = document.querySelector('.white')
 
+// const blueButton = document.querySelector('.blue')
 
-
+// const yellowButton = document.querySelector('.yellow')
 
 // Second, write an event listener for each of the buttons you selected. You can use callback functions or anonymous functions.
 
+// redButton.addEventListener('click', () => {
+//   document.body.className = 'red'
+// })
+
+// whiteButton.addEventListener('click', () => {
+//   document.body.className = 'white'
+// })
+
+// blueButton.addEventListener('click', () => {
+//   document.body.className = 'blue'
+// })
+
+// yellowButton.addEventListener('click', () => {
+//   document.body.className = 'yellow'
+// })
 
 
+// Refactor
 
+const colorCircles = document.querySelectorAll('a')
+// console.log(colorCircles)
+
+colorCircles.forEach((circle) => {
+  circle.addEventListener('click', () => {
+    // console.log(circle.className)
+    document.body.className = circle.className
+  })
+})
 
 
